67c6d0f6a946aeb515ba2f775f6774529ae69939
brunomarcel458 <109152699+brunomarcel458@users.noreply.github.com>
2024-01-26 15:45:37 -0300

    new update

---
+++ novo-plugin.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin name: Novo Plugin
+Plugin URI: #
+Description: Plugin of functions to Wordpress
+Version: 1.0.6
+Author: Bruno Marcel
+Author URI: https://#
+Text Domain: np
+*/
+
+//Se chamar diretamente, aborta
+if( ! defined('WPINC') ){ die(); }
+
+if( file_exists(plugin_dir_path(__FILE__) . 'core-init.php') ){ //verifica se o caminho para o arquivo core-init.php existe
+    require_once(plugin_dir_path(__FILE__) . 'core-init.php'); //executa a função require_once, que serve para chamar o arquivo
+}
