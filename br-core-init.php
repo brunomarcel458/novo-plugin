3b16b78c96870cb073481839aefc085467901ce7
brunomarcel458 <109152699+brunomarcel458@users.noreply.github.com>
2024-01-26 19:52:40 -0300

    organizing repository path

---
+++ br-core-init.php
@@ -0,0 +1,32 @@
+<?php
+
+//Este arquivo inicializa os componentes do plugin
+
+//Se chamar diretamente, aborta
+if( ! defined('WPINC') ){ die(); }
+
+define('BR_CORE_INC', dirname(__FILE__) . '/inc/');
+define('BR_CORE_IMG', plugins_url('img/', __FILE__));
+define('BR_CORE_CSS', plugins_url('css/', __FILE__));
+define('BR_CORE_JS', plugins_url('js/', __FILE__));
+
+//registrando CSS
+function br_register_core_css(){
+    wp_enqueue_style('br-core', BR_CORE_CSS . 'br-core.css', null, time(), 'all');
+}
+add_action('wp_enqueue_scripts', 'br_register_core_css');
+
+//registrando JS/jQuery
+function br_register_core_js(){
+    wp_enqueue_script('br-core', BR_CORE_JS . 'br-core.js', 'jquery', time(), true);
+}
+add_action('wp_enqueue_scripts', 'br_register_core_js');
+
+//includes
+if( file_exists( BR_CORE_INC . 'br-core-functions.php') ){ //verifica se o caminho para o arquivo core-init.php existe
+    require_once BR_CORE_INC . 'br-core-functions.php'; //executa a função require_once, que serve para chamar o arquivo
+}
+
+if( file_exists( BR_CORE_INC . 'br-shortcodes.php') ){ //verifica se o caminho para o arquivo core-init.php existe
+    require_once BR_CORE_INC . 'br-shortcodes.php'; //executa a função require_once, que serve para chamar o arquivo
+}
--- novo-plugin.php
+++ novo-plugin.php
@@ -4,7 +4,7 @@
 Plugin name: Novo Plugin
 Plugin URI: #
 Description: Plugin of functions to Wordpress
-Version: 1.0.6
+Version: 1.0.7
 Author: Bruno Marcel
 Author URI: https://#
 Text Domain: np
@@ -13,6 +13,6 @@ Text Domain: np
 //Se chamar diretamente, aborta
 if( ! defined('WPINC') ){ die(); }
 
-if( file_exists(plugin_dir_path(__FILE__) . 'core-init.php') ){ //verifica se o caminho para o arquivo core-init.php existe
-    require_once(plugin_dir_path(__FILE__) . 'core-init.php'); //executa a função require_once, que serve para chamar o arquivo
+if( file_exists(plugin_dir_path(__FILE__) . 'br-core-init.php') ){ //verifica se o caminho para o arquivo core-init.php existe
+    require_once(plugin_dir_path(__FILE__) . 'br-core-init.php'); //executa a função require_once, que serve para chamar o arquivo
 }
